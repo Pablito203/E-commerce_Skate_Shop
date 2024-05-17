ae4af15c19f1c66fd766a947afc67c2df7ac9def
Pablito203 <pablohrmartins@outlook.com>
2024-05-17 00:15:30 -0300

    Alterar nome, icones e splash screen do aplicativo

--- SkateShopClient/capacitor.config.ts
+++ SkateShopClient/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'SkateShopClient',
+  appName: 'Skate shop',
   webDir: 'www',
   server: {
     androidScheme: 'https'
